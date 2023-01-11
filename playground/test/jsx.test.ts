6cbc3b156b71b7be28e778ceb22bb4657863341e
westhide <mr.westhide@gmail.com>
2023-01-11 11:04:04 +0800

    fix(spell): compile

--- playground/test/jsx.test.ts
+++ playground/test/jsx.test.ts
@@ -1,25 +1,25 @@
-import { describe, it, expect } from "vitest";
+import {describe, expect, it} from "vitest";
 
-import { transformSync } from "@swc/core";
+import {transformSync} from "@swc/core";
 
-function complie(src: string) {
-  return transformSync(src, {
-    isModule: true,
-    jsc: {
-      target: "es2022",
-      parser: {
-        syntax: "typescript",
-        tsx: true,
-      },
-      experimental: {
-        plugins: [["@westhide/swc-plugin-vue-jsx", {}]],
-      },
-    },
-  });
+function compile(src: string) {
+    return transformSync(src, {
+        isModule: true,
+        jsc: {
+            target: "es2022",
+            parser: {
+                syntax: "typescript",
+                tsx: true,
+            },
+            experimental: {
+                plugins: [["@westhide/swc-plugin-vue-jsx", {}]],
+            },
+        },
+    });
 }
 
 describe("SWC Plugin Vue JSX", () => {
-  it("hello world", () => {
-    expect(complie(`const App = <h1>Hello World</h1>;`)).toMatchSnapshot();
-  });
+    it("hello world", () => {
+        expect(compile(`const App = <h1>Hello World</h1>;`)).toMatchSnapshot();
+    });
 });
