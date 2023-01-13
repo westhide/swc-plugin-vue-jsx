ed8b71328b5b2dd80edce20d25b818f11d498341
westhide <mr.westhide@gmail.com>
2023-01-13 16:02:09 +0800

    feat(test): add test cases

--- tests/convert.rs
+++ tests/convert.rs
@@ -4,12 +4,11 @@ use swc_core::{
     ecma::{
         parser::{Syntax, TsConfig},
         transforms::{
-            base::{fixer::fixer, hygiene::hygiene},
+            base::{fixer::fixer, hygiene::hygiene, resolver},
             testing::Tester,
         },
         visit::as_folder,
     },
-    plugin::proxies::PluginCommentsProxy,
 };
 use swc_plugin_vue_jsx::{PluginOptions, VueJSX};
 
@@ -26,13 +25,12 @@ macro_rules! test {
         #[allow(non_snake_case)]
         fn $name() {
             Tester::run(|tester| {
+                let unresolved_mark = Mark::new();
+
                 let module = tester.apply_transform(
                     chain!(
-                        as_folder(VueJSX::<PluginCommentsProxy>::new(
-                            $opts,
-                            None,
-                            Mark::new(),
-                        )),
+                        resolver(unresolved_mark, Mark::new(), false),
+                        as_folder(VueJSX::new($opts, None, unresolved_mark)),
                         hygiene(),
                         fixer(Some(&tester.comments.clone()))
                     ),
@@ -54,11 +52,102 @@ macro_rules! test {
         test!($name, $src, PluginOptions::from("{}"));
     };
 
-    ({ $($name:ident : $src:literal),+ $(,)? }) => {
-        $(test!($name,$src);)+
+    ($($mod:ident:{
+        $($name:ident : $src:literal),+ $(,)?
+    }),+ $(,)?) => {
+        $(
+        #[allow(non_snake_case)]
+        mod $mod {
+            use super::*;
+            $(test!($name,$src);)+
+        }
+        )+
     };
 }
 
-test!({
-    HelloWorld: r#"const app = () => <h1>Hello World</h1>"#
-});
+test!(
+    Tag:{
+        native_div: r#"<div></div>"#,
+        extra_component: r#"let A; <A></A>"#,
+        extra_resolve_component: r#"<A></A>"#,
+        extra_member: r#"<A.b></A.b>"#,
+    },
+    Prop:{
+        r#ref: r#"<div ref={a}></div>"#,
+        key: r#"<div key={a}></div>"#,
+        namespace: r#"<div ns:name={a}></div>"#,
+        spread: r#"<div {...a}></div>"#,
+        spread_with_prop: r#"<div {...a} class="cls"></div>"#,
+        verify_symbol: r#"<div prop-name={a}></div>"#,
+    },
+    Event:{
+        onClick: r#"<div onClick={fn}></div>"#,
+        prefix_on: r#"<div onEvent={fn}></div>"#,
+        namespace: r#"<div on:event={fn}></div>"#,
+    },
+    Directive:{
+        vText: r#"<div v-text="text1"></div>"#,
+        vHtml: r#"<div v-html="<div></div>"></div>"#,
+        vShow: r#"<div v-show={a}></div>"#,
+        vSlots: r#"<div v-slots={slots}></div>"#,
+        vModel: r#"<input v-model={a} />"#,
+        vModel_with_key: r#"<A v-model:key={a} />"#,
+        custom: r#"<div v-custom={a}></div>"#,
+    },
+    Element:{
+        expr_child: r#"<div>{a}</div>"#,
+        spread_child: r#"<div>{...a}</div>"#,
+        jsx_child: r#"<div>{<div></div>}</div>"#,
+        fragment_child: r#"<div>{<></>}</div>"#,
+    },
+    PatchFlag:{
+        dyn_class: r#"<div class={a}></div>"#,
+        dyn_style: r#"<div style={a}></div>"#,
+        dyn_prop: r#"<div prop={a}></div>"#,
+        full_props: r#"<div {...a}></div>"#,
+        hydration_event: r#"<div onEvent={fn}></div>"#,
+        need_patch: r#"<div ref={fn}></div>"#,
+    },
+    Fragment:{
+        children: r#"
+          <>
+            <div></div>
+            <div></div>
+          </>
+        "#,
+    },
+    Text:{
+        clean_text: r#"
+            <div>
+             text1
+             <br/>
+       text2
+             </div>
+        "#,
+    },
+    StaticVNode:{
+        below_threshold: r#"
+          <>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </>
+        "#,
+        above_threshold: r#"
+          <>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </>
+        "#
+    }
+);
+
+test!(
+    Tag_custom_element,
+    r#"<custom-tag></custom-tag>"#,
+    PluginOptions::from(r#"{ "customElementPatterns":["custom-tag"] }"#)
+);
